55230a6738cc869e909450c1820c8111eb1857dd
HanMinSS <gksals6464@naver.com>
2021-12-28 22:10:57 +0900

    💄  header design

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,40 +1,37 @@
 import type { NextPage } from "next";
 import styles from "../styles/Home.module.css";
-
+import MenuImg from "../images/menubar.png";
+import { useState } from "react";
 const Home: NextPage = () => {
   return (
     <>
       {/* header componets */}
       <header className={styles.header}>
-        <section className={styles.nav_wrap}>
-          <a href="#">hanminss</a>
-          <nav>
-            <ul>
+        <section className={styles.navWrap}>
+          <h1 className={styles.logo}>hanminsss</h1>
+          <nav className={styles.nav}>
+            <ul className={styles.linkList}>
               <li>
-                <a href="#AboutMe">about me</a>
+                <a href="#">About me</a>
               </li>
               <li>
-                <a href="#TechSkills">tech skills</a>
+                <a href="#">Skills</a>
               </li>
               <li>
-                <a href="#Projects">projects</a>
+                <a href="#">Projects</a>
               </li>
             </ul>
           </nav>
         </section>
       </header>
-      {/* section component */}
-      <section>
-        <div></div>
-      </section>
       {/* main compomnet */}
       <main>
         {/* about me component */}
-        <section id="AboutMe"></section>
+        <section className={styles.aboutMe}></section>
         {/* skills component */}
-        <section id="TechSkills"></section>
+        <section className={styles.skills}></section>
         {/*  projects component */}
-        <section id="Projects"></section>
+        <section className={styles.projects}></section>
       </main>
       <footer>
         <p>&copy; hanminsss</p>
